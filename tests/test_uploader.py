d1c67fd0c1c888c8e428f323fb35cae63a07585e
Adedayo Ayeni <dedayoa@gmail.com>
2021-04-22 01:19:45 -0600

    tests

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -1,5 +1,6 @@
 import pytest
-from ffmpeg_chainsaw.uploader import UploadTransmitter, requests, HTTPBasicAuth, encoder, http_upload_callback
+from ffmpeg_chainsaw.uploader import UploadTransmitter, boto3, \
+    requests, HTTPBasicAuth, encoder, http_upload_callback, os
 import tempfile
 
 class TestUploadTransmitter:
@@ -135,3 +136,93 @@ class TestUploadTransmitter:
         )
             
         mock_send_update.assert_called_with('INFO', 'file uploaded successfully with http', update_callback_data)
+
+    def test_upload_s3(self, mocker):
+        access_key_id = "accessKeyId"
+        secret_access_key = "secretAccessKey"
+        bucket_name = "bucketName"
+        extra_args = None
+        update_callback_data = {"url": "http://updatedest.net/update.cgi"}
+        destination = {"protocol": "http", "configuration": {
+            "awsAccessKeyId": access_key_id,
+            "awsSecretAccessKey": secret_access_key,
+            "bucketName": bucket_name,
+        }}
+        f = tempfile.NamedTemporaryFile(delete=False)
+        upload_transmitter = UploadTransmitter(self.file_name, self.file_mimetype, destination)
+        upload_transmitter.update_callback_data = update_callback_data
+        
+        mock_boto3_client = mocker.patch('ffmpeg_chainsaw.uploader.boto3.client')
+        mock_send_update = mocker.patch('ffmpeg_chainsaw.uploader.send_update')
+        
+        upload_transmitter.upload_s3(f)
+        
+        mock_boto3_client.assert_called_with(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key
+        )
+        mock_send_update.assert_called_with('INFO', 'file uploaded successfully with s3', update_callback_data)
+
+    def test_upload_sftp(self, mocker):
+        update_callback_data = {"url": "http://updatedest.net/update.cgi"}
+        host = "1.1.1.1"
+        port = "2222"
+        directory = "/store"
+        username = "user1"
+        password = "pass"
+        destination = {"protocol": "sftp", "configuration": {
+            "host": host,
+            "port": port,
+            "directory": directory,
+            "username": username,
+            "password": password
+        }}
+        f = tempfile.NamedTemporaryFile(delete=False)
+        upload_transmitter = UploadTransmitter(self.file_name, self.file_mimetype, destination)
+        upload_transmitter.update_callback_data = update_callback_data
+        mock_send_update = mocker.patch('ffmpeg_chainsaw.uploader.send_update')
+
+        mock_sftp_connection_options = mocker.patch('ffmpeg_chainsaw.uploader.pysftp.CnOpts')
+        mock_sftp_connection_options.host_keys.return_value = None
+
+        mock_sftp_connection = mocker.patch('ffmpeg_chainsaw.uploader.pysftp.Connection')
+
+        upload_transmitter.upload_sftp(f)
+
+        mock_sftp_connection.assert_called_with(
+            host,
+            port=port,
+            username=username,
+            password=password,
+            cnopts=mock_sftp_connection_options()
+        )
+        mock_send_update.assert_called_with('INFO', 'file uploaded successfully with sftp' , update_callback_data)
+
+
+    def test_copy_disk(self, mocker):
+        directory = "/tmp/final"
+        destination = {"protocol": "disk", "configuration": {
+            "directory": directory
+        }}
+        update_callback_data = {"url": "http://updatedest.net/update.cgi"}
+        
+        f = tempfile.NamedTemporaryFile(delete=False)
+        upload_transmitter = UploadTransmitter(self.file_name, self.file_mimetype, destination)
+        upload_transmitter.update_callback_data = update_callback_data
+        mock_send_update = mocker.patch('ffmpeg_chainsaw.uploader.send_update')
+        
+        mock_isdir = mocker.patch('ffmpeg_chainsaw.uploader.os.path.isdir')
+        mock_isdir.return_value = True
+
+        mock_copy = mocker.patch('ffmpeg_chainsaw.uploader.shutil.copyfile')
+
+        upload_transmitter.copy_disk(f)
+
+        basename = os.path.basename(f.name)
+        dest_path = os.path.join(directory, basename)
+
+        mock_copy.assert_called_with(
+            f.name, dest_path
+        )
+        mock_send_update.assert_called_with('INFO', 'file written to disk successfully', update_callback_data)
